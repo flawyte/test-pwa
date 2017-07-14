a0c727c5f36624430cd9c4258a6f1c49e76c4853
Mickael Allonneau <mickael.allonneau@gmail.com>
2017-07-14 18:45:52 +0200

    [sw] all resources are now cached at fetch time

--- sw.js
+++ sw.js
@@ -1,10 +1,39 @@
+const version = 'v1';
+
 this.addEventListener('activate', (event) => {
   console.log('activate');
   console.warn('Refresh to update!');
+
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== version) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
 });
 
 this.addEventListener('fetch', (event) => {
-  console.log('fetch');
+  event.respondWith(
+    caches.match(event.request).then((value) => {
+      console.log('"' + event.request.url + '" is cached => ' + String(value !== undefined).toUpperCase())
+
+      if (value)
+        return value;
+
+      return fetch(event.request).then(function(response) {
+        if (/chrome-extension/.test(event.request.url))
+          return fetch(event.request);
+
+        return caches.open(version).then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });
 
 this.addEventListener('install', (event) => {
