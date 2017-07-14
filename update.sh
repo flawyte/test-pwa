0bfad62c8ebf55e143d5eb3871c3af502759eb97
Mickael Allonneau <mickael.allonneau@gmail.com>
2017-07-14 17:05:16 +0200

    update

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rsync -az --delete --exclude=".git" --exclude="update.sh" ../test-pwa/build/default/ .
-git add -A && git cm -m "-" && git push origin gh-pages
+git add -A && git cm -m "update" && git push origin gh-pages
 
