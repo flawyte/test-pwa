c13e2897abf4f91220613d21b884c8bf38758dbd
Mickael Allonneau <mickael.allonneau@gmail.com>
2017-07-26 18:09:16 +0200

    [five-hundred-px-service] fetch() now requests results sorted by 'created_at'

--- src/core/services/five-hundred-px-service.js
+++ src/core/services/five-hundred-px-service.js
@@ -9,7 +9,7 @@ function wait(ms) {
 class FiveHundredPxService {
   fetch() {
     return fetch(
-      'https://api.500px.com/v1/photos/search?consumer_key=atEYagb4vsDYXWzKdBPeyBI301bpnGXgKMNDQ4Kk&exclude_nude=1&image_size=1080&tag=mountains'
+      'https://api.500px.com/v1/photos/search?consumer_key=atEYagb4vsDYXWzKdBPeyBI301bpnGXgKMNDQ4Kk&exclude_nude=1&image_size=1080&sort=created_at&tag=mountains'
     ).then(response => {
       return response.json().then(json => json.photos);
     });
